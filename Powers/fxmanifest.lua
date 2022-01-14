786c2631ee1ed25532c29d13a98d63c4b831137f
Swift Designs <57051852+SWIFTDESIGNS@users.noreply.github.com>
2022-01-14 00:34:48 +0000

    Add files via upload

---
+++ Powers/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'SwiftDesigns'
+description 'Powers Menu'
+
+ui_page "html/index.html"
+
+client_script 'client.lua'
+
+server_script 'server.lua'
+
+files {
+    "html/index.html",
+    "html/style.css",
+    "html/reset.css",
+    "html/listener.js",
+}
+
